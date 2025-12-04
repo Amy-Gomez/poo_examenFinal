29875b2667352cf04ac515274ea37fb67169f75e
Amy <agomezm15@est.ups.edu.ec>
2025-12-04 15:24:00 -0500

    Resuelve advertencia de serialVersionUID en excepciones personalizadas

--- src/ec/edu/ups/buscaminas/util/JugadaInvalidaException.java
+++ src/ec/edu/ups/buscaminas/util/JugadaInvalidaException.java
@@ -1,7 +1,7 @@
 package ec.edu.ups.buscaminas.util;
 
 public class JugadaInvalidaException extends Exception {
-    
+	private static final long serialVersionUID = 1L;
     // Constructor que recibe el mensaje de error
     public JugadaInvalidaException(String message) {
         super(message);
