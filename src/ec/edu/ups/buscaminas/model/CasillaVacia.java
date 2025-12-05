d6fdb34b4b8c3e391b10725eacdc22cd0b666b99
romels1999 <romelsmarcelo@gmail.com>
2025-12-05 17:58:07 -0500

    CasillaVacia

--- src/ec/edu/ups/buscaminas/model/CasillaVacia.java
+++ src/ec/edu/ups/buscaminas/model/CasillaVacia.java
@@ -1,29 +1,21 @@
 package ec.edu.ups.buscaminas.model;
 
-//Se define las casillas seguras y la logica de los numeros.
 public class CasillaVacia extends Casilla {
-    
-    // Se añade el serialVersionUID, aunque hereda Serializable de Casilla
-    private static final long serialVersionUID = 1L; 
+
+    private static final long serialVersionUID = 1L;
 
     private int minasAlrededor;
 
     public CasillaVacia() {
-        super(); 
+        super();
         this.minasAlrededor = 0;
     }
 
-    public int getMinasAlrededor() {
-        return minasAlrededor;
-    }
-
-    public void setMinasAlrededor(int minasAlrededor) {
-        this.minasAlrededor = minasAlrededor;
-    }
+    public int getMinasAlrededor() { return minasAlrededor; }
+    public void setMinasAlrededor(int minasAlrededor) { this.minasAlrededor = minasAlrededor; }
 
     @Override
     public String obtenerSimbolo() {
-        // Si no hay minas alrededor, muestra un espacio. De lo contrario, muestra el número.
-        return (minasAlrededor == 0) ? " " : String.valueOf(minasAlrededor);
+        return minasAlrededor == 0 ? " " : String.valueOf(minasAlrededor);
     }
 }
