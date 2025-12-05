72a941adc656299f02f92ea67e515bb4f1229969
romels1999 <romelsmarcelo@gmail.com>
2025-12-05 17:57:46 -0500

    Update CasillaMina.java

--- src/ec/edu/ups/buscaminas/model/CasillaMina.java
+++ src/ec/edu/ups/buscaminas/model/CasillaMina.java
@@ -1,18 +1,11 @@
 package ec.edu.ups.buscaminas.model;
 
-// Aqui definimos el comportamiento de uan mina
 public class CasillaMina extends Casilla {
 
-    // Se añade el serialVersionUID, aunque hereda Serializable de Casilla
-    private static final long serialVersionUID = 1L; 
-
-    public CasillaMina() {
-        super();
-    }
+    private static final long serialVersionUID = 1L;
 
     @Override
     public String obtenerSimbolo() {
-        // Muestra "X" cuando se descubre una mina (Game Over)
-        return "X"; 
+        return "X";
     }
 }
